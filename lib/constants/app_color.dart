4dfea7c74450db25da2e930618629f734e209375
WilliamNha <nhawilliam@gmail.com>
2022-11-04 20:20:35 +0700

    udpate

--- lib/constants/app_color.dart
+++ lib/constants/app_color.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static const primaryColor = Color(0xff0B554B);
+  // static const primaryColor = Color(0xffF46620);
   static const cardColor = Color(0xffF2F2F2);
   static const backgroundCardColor = Color(0xffFFFFFF);
   static const secondaryColor = Color(0xffF9C332);
